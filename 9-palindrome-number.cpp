5440603a034d5ac7107c72824bddbcadfc84fc39
michael popp <michael.christopher.popp@gmail.com>
2021-11-14 10:04:40 -0600

    Added further documentation to problem 9

--- 9-palindrome-number.cpp
+++ 9-palindrome-number.cpp
@@ -9,17 +9,18 @@
 
 bool isPalindrome(int x){
 	
+	//  all negative-cases return false
 	if (x < 0){ return false; }
 
+	//  converting integer into a string for array index accessing
 	auto str_x = std::to_string(x);
 	auto size = str_x.length();
 	auto max_index = size - 1;
-
-	for(auto i = 0; i < size; i++){ 
 	
+	//  comparing the first half of indexes with its symmetrical counterpart
+	for(auto i = 0; i < size; i++){ 
 		if(i == max_index - i){ return true; } 
 		if(str_x[i] != str_x[max_index - i]){ return false; } 
-
 	}
 
 	return true;
@@ -28,7 +29,7 @@ bool isPalindrome(int x){
 
 int main(int argc, char* argv[]) {
 
-	if (argc != 2){fprintf(stderr, "USAGE: %s number\n", argv[0]); exit(-1);}
+	if (argc != 2){fprintf(stderr, "USAGE: %s number_to_check_for_palindrome\n", argv[0]); exit(-1);}
 	auto num = atol(argv[1]);
 	auto result = isPalindrome(num);
 
