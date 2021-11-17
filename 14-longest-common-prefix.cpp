67063e1b709883ff762e3f0aa7b1bb4732c1acaf
michael <michael.christopher.popp@gmail.com>
2021-11-17 00:55:39 -0600

    Problem 14 finished but not tested

--- 14-longest-common-prefix.cpp
+++ 14-longest-common-prefix.cpp
@@ -10,8 +10,26 @@
 
 std::string longestCommonPrefix(std::vector<std::string>& strs) {
         
-        std::string result;
+        std::string str1 = strs.front();
+        std::string empResult ="";
+        int min = str1.length();
+        
+        for(std::vector<std::string>::iterator it = strs.begin() + 1; it < strs.end(); it++){
 
+                int counter = 0;
+                std::string str2 = *it;
+                
+                if (str1[0] != str2[0]) {return empResult;}
+                for(int i = 0; i < str2.size(); i++){
+
+                        if(i == str1.length() || str1[i] != str2[i]) {
+                                if (counter < min) { min = counter;}
+                                break;
+                        }
+                        counter++;
+                 }
+        }
+        std::string result = str1.substr(0,min);
         return result;
 }
 
