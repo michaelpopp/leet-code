2ba29cf006bf5e8e421438c725de3ae990adf473
michael popp <michael.christopher.popp@gmail.com>
2021-12-04 11:21:39 -0600

    27 solved

--- 27-remove-element.cpp
+++ 27-remove-element.cpp
@@ -1,12 +1,18 @@
+/*
+ * Problem: 27
+ * Difficulty: easy
+ * contributor: michael popp
+ */
+
 #include <iostream>
 #include <vector>
 
-int removeDuplicates(std::vector<int>& nums){
+int removeElement(std::vector<int>& nums, int val){
     
     if(nums.empty()){return 0;}
 
-    for (auto i = nums.begin() + 1; i != nums.end(); i++){
-        if(*i == *(i - 1)){
+    for (auto i = nums.begin(); i != nums.end(); i++){
+        if(*i == val){
             nums.erase(i); 
             i--;
             continue;
@@ -19,12 +25,13 @@ int removeDuplicates(std::vector<int>& nums){
 
 int main(int argc, char *argv[]){
 
-    std::vector<int> testCase = {0,0,1,1,1,2,2,3,3,4};
-
-    auto result = removeDuplicates(testCase);
+    std::vector<int> testCase = {0,1,2,2,3,0,4,2};
+    int val = 2;
+    auto result = removeElement(testCase, val);
 
     std::cout << result << ": ";
     for (const auto& i : testCase){std::cout << i;}
     std:: cout << std::endl;
+
     return 0;
 }
